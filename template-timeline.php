f3eb63927ba26954f606c5c3f3be598042eb6344
admin <admin@Studio9.local>
2016-02-17 16:26:02 +1300

    Started on meet the bridal party template

--- template-timeline.php
+++ template-timeline.php
@@ -9,11 +9,14 @@
   <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
 
-<?php if( have_rows('timeline_entry') ): ?>
+<?php
+	if( have_rows('timeline_entry') ): 
+	$counter = 0;
+?>
 
 	<ul class="uk-list tl-wrapper">
 
-	<?php while( have_rows('timeline_entry') ): the_row(); 
+	<?php while( have_rows('timeline_entry') ): the_row(); $counter++;
 
 		// vars
 		$image = get_sub_field('timeline_photo');
@@ -23,11 +26,15 @@
 
 		$photo = wp_get_attachment_image_src($image, $size);
 
+		$class = ($counter % 2 == 0 ? 'uk-animation-slide-right' : 'uk-animation-slide-left');
+
+		// $class = if ($counter % 2 == 0){ echo 'uk-animation-slide-right'; } else { echo 'uk-animation-slide-left'; };
+
 		?>
 
 		<li class="uk-position-relative tl-row">
 			<img src="<?php echo $photo[0]; ?>" alt="<?php echo $title; ?>" class="uk-border-circle tl-image" />
-			<div class="tl-text">
+			<div class="tl-text" data-uk-scrollspy="{cls:'<?php echo $class; ?>', repeat: true}">
 				<h2 class="uk-h3 tl-title"><?php echo $title; ?></h2>
 			    <?php echo $text; ?>
 		    </div>
