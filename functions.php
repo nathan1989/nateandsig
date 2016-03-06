8535816bd92a13c31bc58f1c1046414f2f39188b
admin <admin@Studio9.local>
2016-03-07 09:48:17 +1300

    Testing custom nav

--- functions.php
+++ functions.php
@@ -15,7 +15,8 @@ $sage_includes = [
   'lib/setup.php',     // Theme setup
   'lib/titles.php',    // Page titles
   'lib/wrapper.php',   // Theme wrapper class
-  'lib/customizer.php' // Theme customizer
+  'lib/customizer.php', // Theme customizer
+  'lib/custom-navwalker.php', // Custom Navwalker
 ];
 
 foreach ($sage_includes as $file) {
--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,7 @@
 <header class="banner">
     <!-- giant image thing goes here (homepage only) -->
     <?php
-    use Roots\Soil\Nav;
+    use Roots\Soil\CustomNav;
 
     if (  is_front_page() ) { ?>
       <div class="uk-container uk-container-center uk-margin-top uk-margin-bottom">
@@ -19,10 +19,10 @@
       <?php
       if (has_nav_menu('primary_navigation')) :
         // wp_nav_menu(['primary_navigation' => 'primary_navigation', 'menu_class' => 'uk-navbar-nav uk-clearfix ns-main-nav']);
-        // wp_nav_menu( array(
-        //   'theme_location' => 'primary_navigation',
-        //   'walker' => new NavWalker(),
-        // ) );
+        wp_nav_menu( array(
+          'theme_location' => 'primary_navigation',
+          'walker' => new NavWalker(),
+        ) );
       endif;
       ?>
         <!-- <ul class="uk-navbar-nav uk-clearfix ns-main-nav">
@@ -41,58 +41,5 @@
 
             </li>
         </ul> -->
-    </nav>
-    <!-- Nav, every page -->
-    <?php
-      $menu_name = 'primary_navigation';
-      $locations = get_nav_menu_locations();
-      $menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
-      $menuitems = wp_get_nav_menu_items( $menu->term_id, array( 'order' => 'DESC' ) );
-    ?>
-
-    <nav class="uk-navbar uk-text-center">
-    <ul class="uk-navbar-nav uk-clearfix ns-main-nav">
-        <?php
-        $count = 0;
-        $submenu = false;
-        foreach( $menuitems as $item ):
-            $link = $item->url;
-            $title = $item->title;
-            // item does not have a parent so menu_item_parent equals 0 (false)
-            if ( !$item->menu_item_parent):
-            // save this id for later comparison with sub-menu items
-            $parent_id = $item->ID;
-
-        ?>
-        <li class="uk-parent<?php if (is_page($title)) echo ' uk-active'; ?>" data-uk-dropdown>
-            <a href="<?php echo $link; ?>">
-                <?php echo $title; ?>
-            </a>
-        <?php endif; ?>
-
-            <?php if ( $parent_id == $item->menu_item_parent ): ?>
-                <?php if ( !$submenu ): $submenu = true; ?>
-                <div class="uk-dropdown uk-dropdown-navbar">
-                <ul class="uk-nav uk-nav-navbar">
-                <?php endif; ?>
-
-                    <li class="item">
-                        <a href="<?php echo $link; ?>" class="title"><?php echo $title; ?></a>
-                    </li>
-
-                <?php if ( !isset($menuitems[ $count + 1 ]) || $menuitems[ $count + 1 ]->menu_item_parent != $parent_id && $submenu ): ?>
-                </ul>
-                </div>
-                <?php $submenu = false; endif; ?>
-
-            <?php endif; ?>
-
-        <?php if ( !isset($menuitems[ $count + 1 ]) || $menuitems[ $count + 1 ]->menu_item_parent != $parent_id ): ?>
-        </li>                           
-        <?php $submenu = false; endif; ?>
-
-    <?php $count++; endforeach; ?>
-
-    </ul>
     </nav>
 </header>
