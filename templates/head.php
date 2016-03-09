17b9e2da0ad272bffd0f87b7b9d52c6e3ed8689e
Nathan Smith <nathan@goodfolk.co.nz>
2016-03-09 21:36:31 +1300

    Added some Google fonts, fixed up headers

--- templates/head.php
+++ templates/head.php
@@ -2,5 +2,6 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href='https://fonts.googleapis.com/css?family=Nunito|Josefin+Sans:400,700|Rambla:400,700' rel='stylesheet' type='text/css'>
   <?php wp_head(); ?>
 </head>
--- templates/page-header.php
+++ templates/page-header.php
@@ -1,5 +1,5 @@
 <?php use Roots\Sage\Titles; ?>
 
-<div class="page-header">
+<div class="uk-container uk-container-center uk-text-center uk-margin-large-top uk-margin-large-bottom">
   <h1><?= Titles\title(); ?></h1>
 </div>
